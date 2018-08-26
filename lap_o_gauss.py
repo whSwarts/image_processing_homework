da9650f683277db918bf43af30c5246e4e48eb82
whSwarts <swarts.willem@gmail.com>
2018-08-26 13:38:27 +0200

    upload3

--- lap_o_gauss.py
+++ lap_o_gauss.py
@@ -2,26 +2,42 @@ import numpy as np
 import scipy.ndimage as im
 import scipy.misc as misc
 import matplotlib.pyplot as plot
+import scipy.signal as signal
 
-def Gaussian_laplacian(x, y, sigma):
-    G = (-1*(x*x + y*y - 2*sigma^2)/sigma^4) * np.exp(-1*(x^2 + y^2)/2*sigma^2)
-    return G*(-1/(np.pi * sigma^2))
-
-
-def generate_mask(length, width):
-    """""/* create empty arrays for coordinates *\""""
-    empty_X = np.empty((length, width), dtype=float)
-    empty_y = np.empty((length, width), dtype=float)
-
-    """populate the arrays with coordinates where (width, lenght)/2 the origin"""
-    oriX = int((width/2)-1)
-    oriY = int((length/2)-1)
-    empty_X[oriX, oriY] = 0
-    empty_y[oriX, oriY] = 0
+def LaplacianOfGaussian(x, y, sigma):
+    power = ((np.power(x,2) + np.power(y,2))/(2* np.power(sigma, 2)))
+    G = np.exp(-1*power)
+    LaplaceTerm = -1 * (1/(np.pi * np.power(sigma, 4))) * (1-power)
+    return LaplaceTerm * G 
 
 
-generate_mask(8, 8)
+def generate_mask_gauss(length, width, sigma):
+    """generates two coordinate matrices for masks"""
+    XCordinate = np.zeros((width, length), dtype=int)
+    Ycoordinate = np.zeros((width, length), dtype= int)
+    gaussianMask = np.zeros((width, length), dtype= int)
 
-Lena = im.imread('lena_grey.png')
+    center = int((width-1)/2)
+    # print(center)
+
+    for i in range(width):
+        for j in range(length):
+            if( j == center and i == center):
+                Ycoordinate[center, center] = 0
+            else:
+                Ycoordinate[i, j] = center - j
+                XCordinate[i, j] = i - center
+
+    XCordinate = np.transpose(XCordinate)
+    Ycoordinate = np.transpose(Ycoordinate)
+
+    """apply the gaussian filter"""
+    for i in range(width):
+        for j in range(width):
+            gaussianMask[j, i] = LaplacianOfGaussian(XCordinate[j, i], Ycoordinate[j, i], sigma)
+
+    return gaussianMask
+
+
+Lena = im.imread('./input/lena_grey.png')
 deviate = np.std(Lena)
-print(deviate)
