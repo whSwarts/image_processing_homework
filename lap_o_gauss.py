36a15ed2909a15033d65db66bd34e3577ef3fefc
whSwarts <swarts.willem@gmail.com>
2018-08-26 15:49:16 +0200

    upload4

--- lap_o_gauss.py
+++ lap_o_gauss.py
@@ -5,17 +5,17 @@ import matplotlib.pyplot as plot
 import scipy.signal as signal
 
 def LaplacianOfGaussian(x, y, sigma):
-    power = ((np.power(x,2) + np.power(y,2))/(2* np.power(sigma, 2)))
+    power = -1*((np.power(x,2) + np.power(y,2))/(2* np.power(sigma, 2)))
     G = np.exp(-1*power)
-    LaplaceTerm = -1 * (1/(np.pi * np.power(sigma, 4))) * (1-power)
+    LaplaceTerm = (np.power(x, 1) + np.power(y, 2) -(2 * np.power(sigma, 2))) /np.power(sigma, 4)
     return LaplaceTerm * G
 
 
 def generate_mask_gauss(length, width, sigma):
     """generates two coordinate matrices for masks"""
-    XCordinate = np.zeros((width, length), dtype=int)
-    Ycoordinate = np.zeros((width, length), dtype= int)
-    gaussianMask = np.zeros((width, length), dtype= int)
+    XCordinate = np.zeros((width, length))
+    Ycoordinate = np.zeros((width, length))
+    gaussianMask = np.zeros((width, length))
 
     center = int((width-1)/2)
     # print(center)
@@ -30,35 +30,45 @@ def generate_mask_gauss(length, width, sigma):
 
     XCordinate = np.transpose(XCordinate)
     Ycoordinate = np.transpose(Ycoordinate)
-
+    print(XCordinate)
     """apply the gaussian filter"""
     for i in range(width):
         for j in range(width):
             gaussianMask[j, i] = LaplacianOfGaussian(XCordinate[j, i], Ycoordinate[j, i], sigma)
-
+    print(gaussianMask)
     return gaussianMask
 
-    def ZeroCrossing(image):
-        (M, N) = image.shape
-        # detect zero crossing by checking values across 8-neighbors on a 3x3 grid
-        temp = np.zeros((M + 2, N + 2))
-        temp[1:-1, 1:-1] = image
-        img = np.zeros((M, N))
-        for i in range(1, M + 1):
-            for j in range(1, N + 1):
-                if temp[i, j] < 0:
-                    # Checking over 8 neighbor grid for change in polarity of the gradient
-                    for x, y in (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1):
-                        if temp[i + x, j + y] > 0:
-                            img[i - 1, j - 1] = 1
+def ZeroCrossing(image):
+    M = image.shape[0]
+    N = image.shape[1]
+    temp = np.zeros((M + 2, N + 2))
+    temp[1:-1, 1:-1] = image
+    img = np.zeros((M, N))
+    for i in range(1, M + 1):
+        for j in range(1, N + 1):
+            if temp[i, j] < 0:
+                # Checking over 8 neighbor grid for change in sign
+                for x, y in (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1):
+                    if temp[i + x, j + y] > 0:
+                        img[i - 1, j - 1] = 1
+    print(img)
+    return img
 
 Lena = im.imread('./input/lena_grey.png')
-deviate = np.std(Lena)
-Lena_mask_7 = generate_mask_gauss(7,7,deviate)
-Lena_mask_13 = generate_mask_gauss(13,13,deviate)
-Lena_mask_25 = generate_mask_gauss(25,25,deviate)
+deviate = im.standard_deviation(Lena)
+# deviate = 1
+Lena_mask_7 = generate_mask_gauss(7, 7, deviate)
+Lena_mask_13 = generate_mask_gauss(13, 13, deviate)
+Lena_mask_25 = generate_mask_gauss(25, 25, deviate)
 
 Lena_7 = signal.convolve2d(Lena, Lena_mask_7)
+Lena_13 = signal.convolve2d(Lena, Lena_mask_13)
+Lena_25 = signal.convolve2d(Lena, Lena_mask_25)
+Lena_7 = ZeroCrossing(Lena_7)
+print(deviate)
 img_plot7 = plot.imshow(Lena_7, cmap='gray')
 plot.show()
-
+img_plot13 = plot.imshow(Lena_13, cmap='gray')
+plot.show()
+img_plot25 = plot.imshow(Lena_25, cmap='gray')
+plot.show()
